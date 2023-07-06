8ab43a3990b5a8641a3cd59b206403b29f5710a7
Fisch37 <seracin4@gmail.com>
2023-07-06 13:50:06 +0200

    Refactored code and implemented catch for non-configured plugins.

--- src/main/java/me/fisch37/betterresourcepack/ReloadPackTask.java
+++ src/main/java/me/fisch37/betterresourcepack/ReloadPackTask.java
@@ -30,6 +30,7 @@ public class ReloadPackTask extends BukkitRunnable {
 
     private static class FetchTask extends BukkitRunnable{
         private final PackInfo packInfo;
+        // Using Boolean for trinary false-null-true state
         private Boolean isSuccessful;
 
         public FetchTask(PackInfo packInfo){
@@ -51,49 +52,59 @@ public class ReloadPackTask extends BukkitRunnable {
         }
     }
 
+    public void start(){
+        this.runTaskTimer(this.plugin,0L,2L);
+    }
+
+
     @Override
     public void run() {
-        if (this.executingTask == null){
-            this.executingTask = new FetchTask(this.packInfo);
-            if (this.sync) this.executingTask.runTask(this.plugin);
-            else this.executingTask.runTaskAsynchronously(this.plugin);
-        }
+        if (this.executingTask == null) startFetchTask();
+        // Success state is null when task still running
         if (this.executingTask.getSuccessState() == null) return;
 
         boolean op_success = this.executingTask.getSuccessState();
-        if (!op_success){
-            sendToAuthor(ChatColor.RED+"Could not fetch resource pack!");
+        if (!op_success) {
+            sendToAuthor(ChatColor.RED + "Could not fetch resource pack!");
             // Logging sync allows me to essentially debug the situation. Intention is that only /reload executes with sync
-            Bukkit.getLogger().warning("[BSP] Could not fetch resource pack in reload task! Sync: "+this.sync);
-        } else{
-            boolean saveSuccessful = false;
-            try {
-                this.packInfo.saveHash();
-                saveSuccessful = true;
-            } catch (IOException e) {
-                sendToAuthor(ChatColor.RED+"Could not save hash! The hash is still updated, but will reset on the next restart.");
-                Bukkit.getLogger().warning("Could not save hash to cache file in reload task. Sync: "+this.sync);
-            }
-            if (!saveSuccessful) return;
-
+            Bukkit.getLogger().warning("[BSP] Could not fetch resource pack in reload task! Sync: " + this.sync);
+        } else if (saveHash()){
             sendToAuthor("Updated pack hash!");
             Bukkit.getLogger().info("[BSP] Updated pack hash");
-            if (this.push && this.packInfo.isConfigured()){
-                sendToAuthor("Pushing update to all players");
-                for(org.bukkit.entity.Player player : Bukkit.getOnlinePlayers()){
-                    player.setResourcePack(
-                            this.packInfo.getUrl().toString(),
-                            this.packInfo.getSha1()
-                    );
-                }
-            }
+            if (this.push && this.packInfo.isConfigured()) {
+                pushPackToPlayers();
+            } else if (this.push) sendToAuthor("Note that no pack is specified, cannot push anything.");
         }
         cancel();
     }
 
-    public void start(){
-        this.runTaskTimer(this.plugin,0L,2L);
+    private void startFetchTask(){
+        this.executingTask = new FetchTask(this.packInfo);
+        if (this.sync) this.executingTask.runTask(this.plugin);
+        else this.executingTask.runTaskAsynchronously(this.plugin);
     }
+
+    private boolean saveHash(){
+        try {
+            this.packInfo.saveHash();
+            return true;
+        } catch (IOException e) {
+            sendToAuthor(ChatColor.RED + "Could not save hash! The hash is still updated, but will reset on the next restart.");
+            Bukkit.getLogger().warning("Could not save hash to cache file in reload task. Sync: " + this.sync);
+            return false;
+        }
+    }
+
+    private void pushPackToPlayers(){
+        sendToAuthor("Pushing update to all players");
+        for (org.bukkit.entity.Player player : Bukkit.getOnlinePlayers()) {
+            player.setResourcePack(
+                    this.packInfo.getUrl().toString(),
+                    this.packInfo.getSha1()
+            );
+        }
+    }
+
 
     private void sendToAuthor(String message){
         if (this.taskAuthor != null) sendMessage(this.taskAuthor,message);
